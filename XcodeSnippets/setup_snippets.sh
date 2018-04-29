937158f33d6efc1aa5f49c31c2ef5c638cc823ce
manajay <manajay.dlj@gmail.com>
2018-04-29 20:09:03 +0800

    update codesnippet sh

--- XcodeSnippets/setup_snippets.sh
+++ XcodeSnippets/setup_snippets.sh
@@ -1,4 +1,9 @@
 RC_HOME=`pwd`
-ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+CodeSnippets_PATH=~/Library/Developer/Xcode/UserData/CodeSnippets
+#如果文件夹不存在，创建文件夹
+if [ ! -d "${CodeSnippets_PATH}" ]; then
+  mkdir ${CodeSnippets_PATH}
+fi
+ln -s ${SRC_HOME}/CodeSnippets ${CodeSnippets_PATH}
 echo "done"
 
